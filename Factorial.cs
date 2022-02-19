cc1a4ae5fa04009dac4b05da62f78d5166e2755b
vetriselvia <vetriselvira@gmail.com>
2022-02-19 07:33:51 -0800

    initial checkin

---
+++ Factorial.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_challenges
+{
+    internal class FactorialClass
+    {
+
+        [Test]
+        public void Run()
+        {
+            var v = Factorial(5);
+            Assert.AreEqual(15, v);
+        }
+
+
+        int Factorial(int n)
+        {
+
+            int result = 0;
+            for (int i = n; i >= 0; i--)
+            {
+                result = result + i;
+            }
+            return result;
+
+        }
+    }
+}
---
+++ HelloWorld.cs
@@ -0,0 +1,16 @@
+using NUnit.Framework;
+
+namespace csharp_challenges
+{
+    public class HelloWorld
+    {
+        
+        [Test]
+        public void Run()
+        {
+            
+        }
+
+
+    }
+}
