8dd83cec8e76de3c2f88b725e16e9a361b7e0224
vetriselvia <vetriselvira@gmail.com>
2022-02-21 18:11:21 -0800

    Added more challenges

---
+++ NumberOfcharacter.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace csharp_challenges
+{
+    internal class NumberOfcharacter
+    {
+        [Test]
+        public void Run()
+        {
+            string str = "Hello";
+            var Return = OccuranceOfElements(str);
+            Dictionary<char, int> dict = new Dictionary<char, int> { ['H']=1,['e']=1,['l']=2,['o']=1};
+
+            Assert.AreEqual(dict, Return);
+        }
+        Dictionary<char, int> OccuranceOfElements (string str)
+        {
+            Dictionary<char, int> dict = new Dictionary<char, int>();
+            for (int i=0; i < str.Length; i++)
+            {
+                
+                foreach(var ch in str)
+                {
+                    if (dict.ContainsKey(ch))
+                    {
+                        dict[ch] = dict[ch] + 1;
+                    }
+                    else
+                    {
+                        dict.Add(ch, 1);
+                    }
+                }
+               
+              
+            }
+            return dict;
+        }
+    }
+}
---
+++ ReverseWordString.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_challenges
+{
+    internal class ReverseWordString
+    {[Test]
+    public void Run()
+        {
+            string str = "my boy";
+         var Return = ReverseWord(str);
+            string Result = "ym yob";
+            Assert.AreEqual(Result, Return);
+        }
+       string ReverseWord(string str)
+        {
+            string strWord = string.Empty;
+            string final = string.Empty;
+            List<string> lst = new List<string>();
+            for(int i=0; i < str.Length; i++)
+            {
+                if(str[i] != ' ')
+                {
+                    strWord = str[i] + strWord;
+
+                }
+                else
+                {
+                    lst.Add(strWord);
+                    strWord = string.Empty;
+
+                }
+            }
+            lst.Add(strWord);
+            foreach(var word in lst)
+            {
+                final = final + word + " ";
+            }
+            final =final.Substring(0, final.Length-1);
+            return final;
+        }
+    }
+}
